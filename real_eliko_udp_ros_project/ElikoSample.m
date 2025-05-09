71e65e7db12e8a2603d71f69fce700f5530f4e95
zichenwang12321 <wangzichenaisha@gmail.com>
2025-05-09 06:13:34 +0200

    vf

--- real_eliko_udp_ros_project/ElikoSample.m
+++ real_eliko_udp_ros_project/ElikoSample.m
@@ -1,33 +1,24 @@
-
 function [ComplexOut] = ElikoSample()
-    while 1
+    while true
         try
             [ZModule, ZPhase] = ElikoRead();
-        catch 
+        catch
             [ZModule, ZPhase] = ElikoRead();
         end
 
-        ComplexOut = zeros(size(ZModule));
+        tempComplex = zeros(size(ZModule));
         for row = 1:size(ZModule, 1)
             for col = 1:size(ZModule, 2)
                 mod = ZModule(row, col);
                 ang = ZPhase(row, col);
                 [X, Y] = pol2cart(deg2rad(ang), mod);
-                ComplexOut(row, col) = complex(X, Y);
+                tempComplex(row, col) = complex(X, Y);
             end
         end
-        break
+
+        % 不做discard 0/负值的处理
+        break;
     end
+
+    ComplexOut = tempComplex; % 只保存第3次数据
 end
-%% 虚拟测试
-% 模拟一个 3x3 的复数矩阵
-   % realPart = rand(3,3) * 10;
-    %imagPart = rand(3,3) * 10;
-    %ComplexOut = complex(realPart, imagPart);
-%end
-%% 
- % 返回一个 4x4 模拟复数矩阵
-   % realPart = rand(4, 4) * 100;     % 实部
-   % imagPart = rand(4, 4) * 100;     % 虚部
-   % ComplexOut = complex(realPart, imagPart);
-%end
